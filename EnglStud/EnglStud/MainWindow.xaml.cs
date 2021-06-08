4cbabc413ade54740ae688796756355d5cf8bbe3
KeepFobbi <52631773+KeepFobbi@users.noreply.github.com>
2021-06-08 18:48:37 +0300

    add entity db lite
    https://sqlitebrowser.org/

--- EnglStud/EnglStud/MainWindow.xaml.cs
+++ EnglStud/EnglStud/MainWindow.xaml.cs
@@ -20,9 +20,71 @@ namespace EnglStud
     /// </summary>
     public partial class MainWindow : Window
     {
+        ApplicationContext db;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            db = new ApplicationContext();
+
+            List<User> users = db.Users.ToList();
+            string str = "";
+            foreach (User user in users)
+                str += "Login: " + user.Login + " | ";
+
+            exampleText.Text = str;
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = textBoxPass.Password.Trim();
+            string rePass = textBoxPassRe.Password.Trim();
+            string email = textBoxEmail.Text.ToLower().Trim();
+
+
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Not corect";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 5)
+            {
+                textBoxPass.ToolTip = "Not corect";
+                textBoxPass.Background = Brushes.DarkRed;
+            }
+            else if (pass != rePass)
+            {
+                textBoxPassRe.ToolTip = "Not corect";
+                textBoxPassRe.Background = Brushes.DarkRed;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Not corect";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+
+                textBoxPass.ToolTip = "";
+                textBoxPass.Background = Brushes.Transparent;
+
+                textBoxPassRe.ToolTip = "";
+                textBoxPassRe.Background = Brushes.Transparent;
+
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Its Ok!");
+                User user = new User(login, email, pass);
+
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
         }
     }
 }
---
+++ EnglStud/EnglStud/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglStud
+{
+    class User
+    {
+        public int id { get; set; }
+        private string login, pass, email;
+
+        public string Login
+        {
+            get { return login; }
+            set { login = value; }
+        } 
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        } 
+        public string Pass
+        {
+            get { return pass; }
+            set { pass = value; }
+        }
+
+        public User() { }
+
+        public User(string login, string email, string pass)
+        {
+            this.login = login;
+            this.pass = pass;
+            this.email = email;
+        }
+    }
+}
