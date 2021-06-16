d8999e1221d4bc0a44f0017f18b8d89de37bd376
KeepFobbi <52631773+KeepFobbi@users.noreply.github.com>
2021-06-16 22:56:16 +0300

    EntityFramework was added

---
+++ EnglStud/EnglStud_Server/Entities(DB)/DbMainContext.cs
@@ -0,0 +1,17 @@
+﻿using EnglStud_Server.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EnglStud_Server
+{
+    class DbMainContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbMainContext() : base("EnSt_Connection") { }        
+    }
+}
--- EnglStud/EnglStud_Server/Program.cs
+++ EnglStud/EnglStud_Server/Program.cs
@@ -5,58 +5,36 @@ using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using EnglStud_Server.Entities;
 
 namespace EnglStud_Server
 {
     class Program
     {
-        static int port = 8005; // порт для приема входящих запросов
+        
         static void Main(string[] args)
         {
-            // получаем адреса для запуска сокета
-            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            //OpenConnection connection = new OpenConnection(); 
+            DbMainContext db = new DbMainContext();
 
-            // создаем сокет
-            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            try
+            User user = new User() { Login = "Admin", Password = "Admin", Email = "admin@ukr.net" };
+            User user2 = new User() { Login = "test", Password = "test", Email = "test@ukr.net" };
+
+            db.Users.Add(user);
+            db.Users.Add(user2);
+            db.SaveChanges();
+            Console.WriteLine("Objects was added...");
+
+            Console.ReadLine();
+
+            var users = db.Users;
+            Console.WriteLine("Object List");
+            foreach (User u in users)
             {
-                // связываем сокет с локальной точкой, по которой будем принимать данные
-                listenSocket.Bind(ipPoint);
-
-                // начинаем прослушивание
-                listenSocket.Listen(10);
-
-                Console.WriteLine("Сервер запущен. Ожидание подключений...");
-
-                while (true)
-                {
-                    Socket handler = listenSocket.Accept();
-                    // получаем сообщение
-                    StringBuilder builder = new StringBuilder();
-                    int bytes = 0; // количество полученных байтов
-                    byte[] data = new byte[256]; // буфер для получаемых данных
-
-                    do
-                    {
-                        bytes = handler.Receive(data);
-                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                    }
-                    while (handler.Available > 0);
-
-                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
-
-                    // отправляем ответ
-                    string message = "ваше сообщение доставлено";
-                    data = Encoding.Unicode.GetBytes(message);
-                    handler.Send(data);
-                    // закрываем сокет
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("{0}.{1} - {2} - {3}", u.Id, u.Login, u.Password, u.Email);
             }
         }
     }
