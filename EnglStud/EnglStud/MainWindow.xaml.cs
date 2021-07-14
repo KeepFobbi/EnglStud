5e91ad2bdd4c4dac0514d93e8af6b86a0d974538
KeepFobbi <52631773+KeepFobbi@users.noreply.github.com>
2021-07-15 00:40:37 +0300

    .gitignore

--- EnglStud/EnglStud/MainWindow.xaml.cs
+++ EnglStud/EnglStud/MainWindow.xaml.cs
@@ -1,10 +1,15 @@
 ﻿using EnglStud.Entities;
+using Nancy.Json;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SQLite;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,9 +29,11 @@ namespace EnglStud
     public partial class MainWindow : Window
     {
         ApplicationContext db;
+        List<Word> words;
         public MainWindow()
         {
             db = new ApplicationContext();
+            words = db.Words.ToList();
             InitializeComponent();
         }
 
@@ -65,10 +72,9 @@ namespace EnglStud
             StartTask_Field.Visibility = Visibility.Visible;
         }
 
-        private void AddRandom_Word_Click(object sender, RoutedEventArgs e) // to do
+        private void AddRandom_Word_Click(object sender, RoutedEventArgs e) // to do this
         {
             Random rnd = new Random();
-            List<Word> words = db.Words.ToList();
             Word word = db.Words.Find(rnd.Next(2, words.Count()));
 
             CollapseAllElements();
@@ -77,9 +83,73 @@ namespace EnglStud
             Translate_Word_Choose.Text = word.Translation;
         }
 
-        private void ListViewItem_MouseUp(object sender, MouseButtonEventArgs e) // Test
+        private void ListViewItem_MouseUp(object sender, MouseButtonEventArgs e) // Test CollapseAllElements();
         {
             CollapseAllElements();
         }
+
+        private void CheckUserWord_Add_Click(object sender, RoutedEventArgs e)
+        {
+            //string str = EnteredUserWord_TextBox.Text;
+            //string kruc = TranslateText1(str);
+            //Console.WriteLine(kruc);
+        }
+
+        //public string TranslateText(string input)
+        //{
+        //    TranslationServiceClient client = TranslationServiceClient.Create();
+        //    TranslateTextRequest request = new TranslateTextRequest
+        //    {
+        //        Contents = { "It is raining." },
+        //        TargetLanguageCode = "fr-FR",
+        //        Parent = new ProjectName(projectId).ToString()
+        //    };
+        //    TranslateTextResponse response = client.TranslateText(request);
+        //    // response.Translations will have one entry, because request.Contents has one entry.
+        //    Translation translation = response.Translations[0];
+        //    Console.WriteLine($"Detected language: {translation.DetectedLanguageCode}");
+        //    Console.WriteLine($"Translated text: {translation.TranslatedText}");
+        //}
+
+        //public string TranslateText1(string input)
+        //{
+        //    // Set the language from/to in the url (or pass it into this function)
+        //    string url = String.Format
+        //    ("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
+        //     "en", "es", Uri.EscapeUriString(input));
+        //    HttpClient httpClient = new HttpClient();
+        //    string result = httpClient.GetStringAsync(url).Result;
+
+        //    // Get all json data
+        //    var jsonData = new JavaScriptSerializer().Deserialize<List<dynamic>>(result);
+
+        //    // Extract just the first array element (This is the only data we are interested in)
+        //    var translationItems = jsonData[0];
+
+        //    // Translation Data
+        //    string translation = "";
+
+        //    // Loop through the collection extracting the translated objects
+        //    foreach (object item in translationItems)
+        //    {
+        //        // Convert the item array to IEnumerable
+        //        IEnumerable translationLineObject = item as IEnumerable;
+
+        //        // Convert the IEnumerable translationLineObject to a IEnumerator
+        //        IEnumerator translationLineString = translationLineObject.GetEnumerator();
+
+        //        // Get first object in IEnumerator
+        //        translationLineString.MoveNext();
+
+        //        // Save its value (translated text)
+        //        translation += string.Format(" {0}", Convert.ToString(translationLineString.Current));
+        //    }
+
+        //    // Remove first blank character
+        //    if (translation.Length > 1) { translation = translation.Substring(1); };
+
+        //    // Return translation
+        //    return translation;
+        //}
     }
 }
